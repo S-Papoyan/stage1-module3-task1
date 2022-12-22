1517a5dd26521da0bcc55126299c884d3df1f24b
Sergey <spapoyan1987@gmail.com>
2022-12-22 12:40:53 +0400

    commit

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -1,27 +1,27 @@
 package com.epam.mjc.generics;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Generics {
+public class Generics<T> {
 
     //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+    public List<List<String>> boxingMethod(String name) {
+        List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
-        secondList.add(firstList);
+        List<List<String>> secondList = new ArrayList<>();
+        secondList.add(Collections.singletonList(firstList.toString()));
         return secondList;
     }
 
     //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    public <E> T genericMethod(T data) {
         return data;
     }
 
     //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+    public void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
-
 }
